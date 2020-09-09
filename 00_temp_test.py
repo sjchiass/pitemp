e43e313a50005f5df187f08d13dcd5d8a539ae36
Sebastien <sjchiass@gmail.com>
2020-09-09 07:44:46 -0400

    Add more documentation

--- 00_temp_test.py
+++ 00_temp_test.py
@@ -9,7 +9,42 @@ from gpiozero import CPUTemperature
 import argparse
 import os
 
-parser = argparse.ArgumentParser(description="Stress test and log temperature")
+"""
+NAME
+      00_test_test.py
+SYNOPSIS
+      A script that makes the RPi CPU work and logs the temperatures
+DESCRIPTION
+      This is a script that will run for --duration minutes, stressing the RPi
+      CPU and logging temperatures every second. The user can then open the
+      saved CSV output in their analysis environment of choice to further
+      explore the thermal behaviour of the Raspberry Pi.
+    
+      The script uses argparse for setting options.
+      
+      The script works by first deciding on a CSV filename and either creating
+      it or appending to it, if it already exists. The script provides
+      arguments for naming the CSVs according to the type of Raspberry Pi case
+      being tested.
+
+      The --max_threads arguments decides on the maximum amount of threads used
+      for the stress command-line tool (stress is a command-line utility for
+      testing Raspberry Pis). The script will randomly select a stress level and
+      will also sometimes decide to sleep in order to simulate rest.
+
+      The script will continue testing until --duration minutes have elapsed.
+"""
+
+parser = argparse.ArgumentParser(
+    description="Stress test Raspberry Pi and log temperature",
+    epilog="""The script will use the --max_threads option to run CPU stress
+    tests for up to max_threads thread, up until it reaches a total runtime of
+    --duration minutes. The temperature data will all be logged to a CSV, with
+    measurements taken every second. The CSV filename itself will depend on the
+    other arguments, allowing you to separate different testing scenarios into
+    different files. If a filename already exists, it will be appended to rather
+    than overwritten.
+    """)
 parser.add_argument("file_prefix", type=str, help="name of file to output")
 parser.add_argument("--duration", type=int, help='total duration in minutes', default=60)
 parser.add_argument("--max_threads", type=int, help='max number of threads', default=4)
@@ -43,7 +78,23 @@ dummy_labels = ["case_under", "case_frame", "case_cable",
                 "top_intake", "top_exhaust", "heatsink_main",
                 "heatsink_sub"]
 
+
 def run_tests(test_time_limit, max_test_threads, csv_writer):
+    """Run and log random CPU stress until the time is up
+
+    Parameters
+    ----------
+    test_time_limit : datetime
+        The function will terminate when this datetime is reached
+    max_test_threads : integer
+        CPU stress tests will be randomly selected from 0 (sleep) up to this value
+    csv_writer : csv.writer
+        A writer object from the csv standard library
+
+    Returns
+    -------
+    void
+    """
     # Previous test level
     # This is saved each iteration so that two load identical load levels
     # do not occur one afer the other
@@ -63,7 +114,6 @@ def run_tests(test_time_limit, max_test_threads, csv_writer):
         # so we can keep doing other stuff
         if stress == 0:
             print(f"sleep {duration} seconds ...")
-            load = 0
             p = subprocess.Popen(["sleep", f"{duration}"])
         else:
             print(f"stress {stress} cpus for {duration} seconds ...")
@@ -87,6 +137,8 @@ if os.path.isfile(file_path):
 else:
     print(f"[Startup] The file {file_path} is a new one. It will be created.")
 
+# Let the user see the target filename
+# There will be a 10 second delay before the script starts
 print(f"[Startup] Tests will be run with {', '.join(active_dummies)}")
 print(f"[Startup] If this is not what you intended, Ctrl+C in the next 10 seconds.")
 time.sleep(10)
@@ -102,7 +154,7 @@ if os.path.isfile(file_path):
 else:
     with open(file_path, mode="w") as f:
         w = csv.writer(f)
-        w.writerow(["datetime", "usage", "temp", "stress", "load"])
+        w.writerow(["datetime", "usage", "temp", "stress"])
         run_tests(test_time_limit=t_end,
                 max_test_threads=args.max_threads,
                 csv_writer=w)
